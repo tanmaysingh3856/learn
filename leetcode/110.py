9bece7a0e5317b43389c9d3f57c80203dbb06aa3
tanmaysingh3856 <tanmaysingh3856@gmail.com>
2024-05-07 17:30:53 +0530

    removed leetcode

--- leetcode/110.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def isBalanced(self, root):
-        def height(node):
-            if not node or abs(height(node.left) - height(node.right)) > 1:
-                return -1
-            return max(height(node.left), height(node.right)) + 1
-
-        return height(root) >= 0
