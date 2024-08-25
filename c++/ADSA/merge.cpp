184d1250f6582c6b5040c238f46229245e200a99
tanmaysingh3856 <tanmaysingh3856@gmail.com>
2024-08-26 00:06:51 +0530

    hehe

--- c++/ADSA/merge.cpp
+++ c++/ADSA/merge.cpp
@@ -5,19 +5,18 @@ using namespace std;
 void merge(int arr[], int l, int m, int r) {
     int n1 = m - l + 1;
     int n2 = r - m;
-    
     int L[n1], R[n2];
-    
+
     for(int i = 0; i < n1; i++) {
         L[i] = arr[l + i];
     }
-    
+
     for(int i = 0; i < n2; i++) {
         R[i] = arr[m + 1 + i];
     }
-    
+
     int i = 0, j = 0, k = l;
-    
+
     while(i < n1 && j < n2) {
         if(L[i] <= R[j]) {
             arr[k] = L[i];
@@ -28,13 +27,13 @@ void merge(int arr[], int l, int m, int r) {
         }
         k++;
     }
-    
+
     while(i < n1) {
         arr[k] = L[i];
         i++;
         k++;
     }
-    
+
     while(j < n2) {
         arr[k] = R[j];
         j++;
@@ -46,12 +45,12 @@ void mergeSort(int arr[], int l, int r) {
     if(l >= r) {
         return;
     }
-    
+
     int m = l + (r - l) / 2;
-    
+
     mergeSort(arr, l, m);
     mergeSort(arr, m + 1, r);
-    
+
     merge(arr, l, m, r);
 }
 
@@ -67,13 +66,13 @@ int main() {
         cin>>arr[i];
     }
     int n = sizeof(arr) / sizeof(arr[0]);
-    
+
     mergeSort(arr, 0, n - 1);
-    
+
     for(int i = 0; i < n; i++) {
         cout << arr[i] << " ";
     }
-    
+
     return 0;
 }
 
