5a5a04d6d1621f2e22dfb5dd2e1a35b6b189c4e4
Tanmay Singh <89771502+tanmaysingh3856@users.noreply.github.com>
2024-09-19 09:17:13 +0530

    hehe

--- py/test.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-import aiohttp
-
-async def _main():
-    url="https://open.spotify.com/search"
-    async with aiohttp.request("GET",url=url) as response:
-        print(await response.text())
-
-asyncio.run(_main())
